1cc809a1f1cf0af40c9f93427d38c7d871429c0b
James Nicolas <jamesnicolas@me.com>
2020-06-07 21:07:24 -0400

    update script to replace the file

--- mixups.py
+++ mixups.py
@@ -1,6 +1,15 @@
+import os
+import shutil
+
+shutil.copyfile('mixups.txt', '.backup.mixups.txt')
+
 with open('mixups.txt', encoding='utf8') as f:
-    for line in f:
-        words = line.split()
-        words.sort()
-        output = '　'.join(words)
-        print(output)
+    with open('tmp.txt', 'w+', encoding='utf8') as o:
+        for i, line in enumerate(f):
+            words = line.split()
+            words.sort()
+            output = ('\n' if i else '') + '　'.join(words)
+            o.write(output)
+
+os.remove('mixups.txt')
+os.rename('tmp.txt', 'mixups.txt')
