e2e169992d57c92d6e58ee129132649d02c90870
James Nicolas <jamesnicolas@me.com>
2020-06-19 19:12:33 -0400

    refactor format.py wip

--- cards/askN1/format.py
+++ cards/askN1/format.py
@@ -2,6 +2,42 @@ import os
 import glob
 import sys
 
+"""
+format.py reads all the .ankle files, puts them into memory, then processes them sequentially.
+Files need to be processed sequentially because some of the fields depend on their previous inputs.
+"""
+
+ankle = {
+    "Vocabulary-Kanji": {
+        "index":0,
+        "func":"$kanji(Vocabulary-Furigana)"
+    },
+    "Vocabulary-Furigana":{"index":1},
+    "Vocabulary-Kana": {
+        "index":2,
+        "func":"$kana(Vocabulary-Furigana)"
+    },
+    "Definition-English":{"index":3},
+    "Definition-Japanese":{"index":4},
+    "Vocabulary-Audio":{"index":5},
+    "Vocabulary-Pos":{"index":6},
+    "Caution":{"index":7},
+    "Sentence-Kanji":{"index":8},
+    "Sentence-Furigana":{"index":9},
+    "Sentence-Kana":{"index":10},
+    "Sentence-English":{"index":11},
+    "Sentence-Clozed":{"index":12},
+    "Sentence-Audio":{"index":13},
+    "Sentence-Image":{"index":14},
+    "Notes":{"index":15},
+    "Core-Index":{"index":16},
+    "Optimized-Voc-Index":{"index":17},
+    "Ask-N1-Index":{"index":18},
+    "Show-JJ":{"index":19},
+    "Show-JE":{"index":20},
+    "Tags":{"index":21},
+}
+
 # get all .ankle files and sort them
 ankles = []
 for file in glob.glob("*.ankle"):
